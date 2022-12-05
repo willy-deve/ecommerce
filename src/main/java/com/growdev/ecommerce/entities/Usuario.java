d9259daefdc8046373d07043c1d93718da0ef737
willy <willydasilva29@gmail.com>
2022-12-05 09:04:52 -0300

    iniciando spring security, adicionando dependecias

--- src/main/java/com/growdev/ecommerce/entities/Usuario.java
+++ src/main/java/com/growdev/ecommerce/entities/Usuario.java
@@ -1,11 +1,16 @@
 package com.growdev.ecommerce.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,7 +19,7 @@ import java.util.Set;
 @EqualsAndHashCode
 @Entity
 @Table(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,4 +38,51 @@ public class Usuario {
     inverseJoinColumns = @JoinColumn(name = "role_id")
   )
   private Set<Roles> roles = new HashSet<>();
+
+
+
+
+
+  //TODOS OS METODOS PARA TRABALHAR COM SEGURANÇA NO SPRING
+
+
+  //Esse metodo é responsavel pela coleção de perfis para o usuario
+  //Esse método que diz ao spring security quem são os niveis de acesso
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+
+    //Percorrendo a List de ROLES e mandando ela CONVERTIDA EM UMA LISTA DE GRANTEDAUTHORITY
+    return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
 }
